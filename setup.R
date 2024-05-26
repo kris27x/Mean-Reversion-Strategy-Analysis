b26d1091bf90efa7f435d636350610b9ad0466d0
kris27x <krzysztof.strzelec27x@gmail.com>
2024-05-26 17:54:44 +0100

    Restored one of the previous setup.R file versions as the latest changes (resolving/suppressing conflicts) caused additional issues

--- setup.R
+++ setup.R
@@ -1,20 +1,15 @@
 # setup.R
 
-# Resolve package conflicts and suppress specific warnings
-conflictRules("dplyr", exclude = c("lag", "filter", "intersect", "setdiff", "setequal", "union"))
-options(xts.warn_dplyr_breaks_lag = FALSE)
+# Ensure the required packages are installed and loaded for a quantitative trading analysis environment
 
 # Function to install and load packages
 install_and_load <- function(packages) {
-  # Identify packages that are not yet installed
   new_packages <- packages[!(packages %in% installed.packages()[, "Package"])]
-  # Install any new packages
   if (length(new_packages)) install.packages(new_packages)
-  # Load all packages, and return the result
   sapply(packages, require, character.only = TRUE)
 }
 
-# CRAN packages required for quantitative trading analysis environment
+# Packages to be installed from CRAN
 cran_packages <- c("quantmod", "TTR", "PerformanceAnalytics", "xts", "zoo", "dplyr", "ggplot2", 
                    "tidyquant", "lubridate", "caret", "data.table", "readr", "httr", "jsonlite", 
                    "ROI", "forecast", "nloptr", "rmarkdown", "bookdown", "knitr", "ggcorrplot", 
@@ -43,40 +38,6 @@ library(quantstrat)
 
 # Load all other libraries
 sapply(cran_packages, library, character.only = TRUE)
-
-# Function to resolve package conflicts by explicitly calling the correct function
-resolve_conflicts <- function() {
-  # Base package conflicts
-  base_conflicts <- c("date", "intersect", "setdiff", "setequal", "union")
-  stats_conflicts <- c("filter", "lag")
-  xts_conflicts <- c("first", "last")
-  dplyr_conflicts <- c("between", "first", "last")
-  data.table_conflicts <- c("hour", "isoweek", "mday", "minute", "month", "quarter", "second", "wday", "week", "yday", "year")
-  zoo_conflicts <- c("yearmon", "yearqtr")
-  
-  # Resolve conflicts by reassigning functions from namespaces to global environment
-  for (conflict in base_conflicts) {
-    assign(conflict, get(conflict, envir = asNamespace("base")), envir = .GlobalEnv)
-  }
-  for (conflict in stats_conflicts) {
-    assign(conflict, get(conflict, envir = asNamespace("stats")), envir = .GlobalEnv)
-  }
-  for (conflict in xts_conflicts) {
-    assign(conflict, get(conflict, envir = asNamespace("xts")), envir = .GlobalEnv)
-  }
-  for (conflict in dplyr_conflicts) {
-    assign(conflict, get(conflict, envir = asNamespace("dplyr")), envir = .GlobalEnv)
-  }
-  for (conflict in data.table_conflicts) {
-    assign(conflict, get(conflict, envir = asNamespace("data.table")), envir = .GlobalEnv)
-  }
-  for (conflict in zoo_conflicts) {
-    assign(conflict, get(conflict, envir = asNamespace("zoo")), envir = .GlobalEnv)
-  }
-}
-
-# Resolve conflicts after loading all packages
-resolve_conflicts()
 
 # Print confirmation message
 cat("All packages have been installed and loaded successfully.\n")
